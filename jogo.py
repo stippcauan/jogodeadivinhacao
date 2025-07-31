be3f2e3d053fa74ac99000fb8a0e0f82fbebc563
stippcauan <cauan.stipp@escola.pr.gov.br>
2025-07-31 12:17:50 +0000

    jogo.py primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,10 @@
+import random
+print("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩")
+print("🟩  jogo de adivinhação  I")
+print("🟩                       I")
+print("🟩         CAUAN         I")
+print("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩")
+
+numeroSecreto= random.randrange(0,100)
+totalTentativas = 0
+pontos = 100

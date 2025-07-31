2e3ea836879a55d089c657923070bebd43707f27
stippcauan <cauan.stipp@escola.pr.gov.br>
2025-07-31 14:49:50 +0000

    jogo.py parte 2

--- jogo.py
+++ jogo.py
@@ -1,6 +1,6 @@
 import random
 print("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩")
-print("🟩  jogo de adivinhação  I")
+print("🟩   JOGO DE AVALIAÇÃO   I")
 print("🟩                       I")
 print("🟩         CAUAN         I")
 print("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩")
@@ -8,3 +8,22 @@ print("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩")
 numeroSecreto= random.randrange(0,100)
 totalTentativas = 0
 pontos = 100
+
+print("Qual o níveis de dificuldade?")
+print("")
+print("(1)-Fácil (2)- Médio (3)- Difícil ")
+print("")
+nível = int(input("Escolha um nível"))
+print("")
+if (nível == 1):
+    print(" Você tem 20 tentativas IXE É NOVATO CAGÃO")
+    totalTentativas = 20
+elif (nível == 2):
+    print(" Você tem 10 tentativasOLHA JA TEM UM POUCO DE CORRAGEM ")
+    totalTentativas = 10
+elif (nível == 3):
+    print("Você tem 5 tentativas Ó AI SIM VETERANO CORAJOSO ")
+    totalTentativas = 5
+else:
+    print("número invalido")
+    totalTentativas = 0
